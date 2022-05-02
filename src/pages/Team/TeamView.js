cada1a4613af85888cff6e565a46a72f41acb712
MathieuGery <mathieu.gery@epitech.eu>
2022-05-03 00:09:23 +0200

    Add team view

--- src/pages/Team/TeamView.js
+++ src/pages/Team/TeamView.js
@@ -1,5 +1,26 @@
 import NavBar from "../../components/NavBar";
-import HeroSection from "../Home/Components/HeroSection";
+import MemberComponent from "./Components/Member";
+
+/* This example requires Tailwind CSS v2.0+ */
+const people = [
+  {
+    name: 'Whitney Francis',
+    role: 'Copywriter',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    twitterUrl: '#',
+    linkedinUrl: '#',
+  },
+  {
+    name: 'Whitney Francis',
+    role: 'Copywriter',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    twitterUrl: '#',
+    linkedinUrl: '#',
+  },
+  // More people...
+]
 
 export default function Team() {
   return (
@@ -9,8 +30,26 @@ export default function Team() {
           <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
             <NavBar />
           </div>
+          <div className="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-24">
+            <div className="space-y-12">
+              <div className="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
+                <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">Meet our team</h2>
+                <p className="text-xl text-gray-500">
+                  Ornare sagittis, suspendisse in hendrerit quis. Sed dui aliquet lectus sit pretium egestas vel mattis
+                  neque.
+                </p>
+              </div>
+              <ul
+                role="list"
+                className="mx-auto space-y-16 sm:grid sm:grid-cols-2 sm:gap-16 sm:space-y-0 lg:grid-cols-3 lg:max-w-5xl"
+              >
+                {people.map((person) => (<MemberComponent person={person} />))}
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </>
   )
 }
+
