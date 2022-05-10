5a3b95e0ab25ffe141cb65d16b1d90fa38082b83
MathieuGery <mathieu.gery@epitech.eu>
2022-05-10 10:16:09 +0200

    Add env variable

--- src/config/api.js
+++ src/config/api.js
@@ -1,1 +1,2 @@
 export const BASE_URL = 'https://strapi.gery.me';
+export const API_TOKEN = process.env.REACT_APP_API_TOKEN
--- src/services/teamServices.js
+++ src/services/teamServices.js
@@ -1,6 +1,7 @@
 import { SYSTEM_ERROR } from "../constants/apiEndpoints";
 import axios from 'axios'
 import { GET_ALL_TEAM_MEMBERS } from "../constants/apiEndpoints";
+import { API_TOKEN } from "../config/api";
 
 /**
  * Function to fetch all the team members.
@@ -13,7 +14,7 @@ export const getAllTeamMembers = () => {
       axios
         .get(GET_ALL_TEAM_MEMBERS, {
           headers: {
-            Authorization: `Bearer 6621d2372306b3d11d92857f724c3ee37faaa28dbeec6953e844bab1686d3f6f8423469774dce4f07ad64145ff927e1cb82a6dad0b708ac9d627b3b436a5b7bf8becbed135f4d58d062548ccbabda691ef5fc9b3fdfd01caf599b1e296949554a107c7fa5cd3df611b20eda853fad11bc60f2447c0f728a9f189345b0ec51505`
+            Authorization: `Bearer ` + API_TOKEN
           }
         })
         .then((res) => {
